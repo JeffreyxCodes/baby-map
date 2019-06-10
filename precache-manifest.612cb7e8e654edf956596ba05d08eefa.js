2c44d13e4127bcdf8a147532ee26931020c28b7b
jeffreyxdc <jeffreyxdc@gmail.com>
2019-06-10 00:41:29 -0400

    Updates

---
+++ precache-manifest.612cb7e8e654edf956596ba05d08eefa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78bdf87eac5d821d7b032606bd6f699",
+    "url": "/baby-map/index.html"
+  },
+  {
+    "revision": "281c3422e1cd7109b72c",
+    "url": "/baby-map/static/css/main.497ecfd5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/baby-map/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "281c3422e1cd7109b72c",
+    "url": "/baby-map/static/js/main.53dad5ef.chunk.js"
+  },
+  {
+    "revision": "8213b06e36a32a8d69b3",
+    "url": "/baby-map/static/js/runtime~main.7c25e825.js"
+  }
+]);
